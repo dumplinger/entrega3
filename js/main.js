4f5ad9e5bae9ad0e26b13a826cd150b07cc7c04d
dumplinger <milena.d.industrial@gmail.com>
2021-08-02 22:32:17 -0300

    first committt

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const pochocla = prompt("¿Quien es la pochocla mas linda, hermosa, maravillosa del universo?")
+
+switch(pochocla) {
+    case "rocio":
+      alert(`SIII ${pochocla} es la pochocla mas linda, hermosa, maravillosa del universo 😍🥰`);
+      break;
+    case "Rocio":
+      alert(`TAL CUAL!!!! ${pochocla} es todo lo que esta bien en este universo mundial 😍🥰`);
+      break;
+    case "Rocío":
+        alert(`NUNCA TAN CIERTO !!! Ella es la luz de mis días 😍🥰`);
+      break;
+    case "rocío":
+        alert(`NUNCA TAN CIERTO !!! Ella es realmente la definición de pochoclita hermosa 😍🥰`);
+    break;
+    default:
+        alert(`Claramente no te rescataste que Rocío es la mejor pochoclita del universo, te recomiendo que te informes puta 😍🥰`);
+      break;
+   }
+   
